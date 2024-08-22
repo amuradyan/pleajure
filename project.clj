ac62f4fc171f3d43eaeb58902b1253999d2297c6
darthspectrum <muradianator@gmail.com>
2024-08-22 20:08:18 +0000

    chore: playing around with test runners

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject pleajure "0.1.0-SNAPSHOT"
   :description "A config tool in clojure"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"] [eftest "0.6.0"]]
   :main ^:skip-aot pleajure.core
   :target-path "target/%s"
-  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.25.0"]]}})
+  :profiles {:dev {:plugins [[lein-eftest "0.6.0"]
+                             [com.jakemccrary/lein-test-refresh "0.25.0"]]}})
+
