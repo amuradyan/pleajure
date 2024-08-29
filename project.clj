470f24c75c7027603f68cd48e13f7761fb1de290
darthspectrum <muradianator@gmail.com>
2024-08-29 21:27:53 +0000

    chore: less noise in the project file
    * `lein deploy` defaults to clojars

--- project.clj
+++ project.clj
@@ -5,24 +5,8 @@
             :url          "https://opensource.org/license/0bsd"
             :distribution :repo}
   :scm {:name "git" :url "https://github.com/amuradyan/pleajure"}
-  :pom-addition ([:developers
-                  [:developer
-                   [:id "amuradyan"]
-                   [:name "Andranik Muradyan"]
-                   [:url "https://github.com/amuradyan"]
-                   [:roles
-                    [:role "developer"]
-                    [:role "maintainer"]]]])
-  :classifiers [["sources" {:source-paths      ^:replace []
-                            :resource-paths    ^:replace ["javadoc"]}]
-                ["javadoc" {:source-paths      ^:replace []
-                            :resource-paths    ^:replace ["javadoc"]}]]
   :dependencies [[org.clojure/clojure "1.11.1"]]
-  :target-path "target/%s"
-  :profiles {:dev {:plugins [[lein-eftest "0.6.0"]
-                             [com.jakemccrary/lein-test-refresh "0.25.0"]]}}
-  :deploy-repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-                                     :creds :gpg}
-                         "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
-                                      :creds :gpg}]])
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.25.0"]]}}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]])
 
